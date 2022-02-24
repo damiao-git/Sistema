36d0939054fe6e114af250369b8c7fcc105307ff
Damiao <damiao.jose@hotmail.com>
2022-02-24 15:31:21 -0300

    Criando o Department

---
+++ ServicesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServicesWebMvc.Models;
+
+namespace ServicesWebMvc.Controllers
+    {
+    public class DepartmentsController : Controller
+        {
+        public IActionResult Index()
+            {
+            List<Department> listDep = new List<Department>();
+            listDep.Add(new Department(1, "Cars"));
+            listDep.Add(new Department(2, "Motorcycles"));
+
+            return View(listDep);
+            }
+        }
+    }
---
+++ ServicesWebMvc/Models/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicesWebMvc.Models
+    {
+    public class Department
+        {
+        public int Id { get; set; }
+        public String Name { get; set; }
+
+        public Department(int id, string name)
+            {
+            Id = id;
+            Name = name;
+            }
+
+        public override string ToString()
+            {
+            return base.ToString();
+            }
+        }
+    }
