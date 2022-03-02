399b6a49e2017cfd31a6740379a5a228ccc41f82
Damiao <damiao.jose@hotmail.com>
2022-03-02 15:48:11 -0300

    detalhes no bootstrap e add novas classes

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+    {
+    public class Seller
+        {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthTime { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+            {
+            }
+
+        public Seller(int id, string name, string email, DateTime birthTime, double baseSalary, Department department)
+            {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthTime = birthTime;
+            BaseSalary = baseSalary;
+            Department = department;
+            }
+
+        public void AddSales(SalesRecord sr)
+            {
+            Sales.Add(sr);
+            }
+        public void RemoveSales(SalesRecord sr)
+            {
+            Sales.Remove(sr);
+            }
+
+        public double TotalSales(DateTime initial, DateTime final)
+            {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+            }
+        }
+    }
