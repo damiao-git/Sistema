251c2f1b23cf96a61a7fc29cc87742ed9ef41361
Damiao <damiao.jose@hotmail.com>
2022-03-04 00:11:45 -0300

    Criação do select de departamentos no Sellers

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -5,16 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Services;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
     {
     public class SellersController : Controller
         {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
             {
             _sellerService = sellerService;
+            _departmentService = departmentService;
             }
 
         public IActionResult Index()
@@ -24,7 +27,9 @@ namespace SalesWebMvc.Controllers
             }
         public IActionResult Create()
             {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments};
+            return View(viewModel);
             }
         [HttpPost]
         [ValidateAntiForgeryToken]
