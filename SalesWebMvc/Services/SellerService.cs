3e7da20479b2265a4ab86e9480f0965506834c8d
Damiao <damiao.jose@hotmail.com>
2022-03-03 23:05:47 -0300

    continuação CRUD parte Create

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -19,5 +19,10 @@ namespace SalesWebMvc.Services
             {
             return _context.Seller.ToList();
             }
+        public void Insert(Seller obj)
+            {
+            _context.Add(obj);
+            _context.SaveChanges();
+            }
         }
     }
