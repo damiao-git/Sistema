5ed7bd35c654ac692712088de080c4c9dce48811
Damiao <damiao.jose@hotmail.com>
2022-03-04 23:22:14 -0300

    Deletar vendedor + tela de confirmação

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -38,5 +38,25 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
             }
+        public IActionResult Delete(int? id)
+            {
+            if(id == null)
+                {
+                return NotFound();
+                }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+                {
+                return NotFound();
+                }
+
+            return View(obj);
+            }
+        [HttpPost]
+        public IActionResult Delete(int id)
+            {
+            _sellerService.Remove(id);
+                return RedirectToAction(nameof(Index));
+            }
         }
     }
