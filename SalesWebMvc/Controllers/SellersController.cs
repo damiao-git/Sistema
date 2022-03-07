6eeab9ec3b966ebc6147ba56e4fe9e72d1c6ea9a
Damiao <damiao.jose@hotmail.com>
2022-03-07 12:27:12 -0300

    Tela de detalhes+edição+Exceções

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using SalesWebMvc.Services;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
     {
@@ -57,6 +58,57 @@ namespace SalesWebMvc.Controllers
             {
             _sellerService.Remove(id);
                 return RedirectToAction(nameof(Index));
+            }
+        public IActionResult Details(int? id)
+            {
+            if (id == null)
+                {
+                return NotFound();
+                }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+                {
+                return NotFound();
+                }
+
+            return View(obj);
+            }
+        public IActionResult Edit(int? id)
+            {
+            if (id == null)
+                {
+                return NotFound();
+                }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+                {
+                return NotFound();
+                }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+            }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+            {
+            try {
+                if (id != seller.Id)
+                    {
+                    return BadRequest();
+                    }
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+                }
+            catch(NotFoundException e)
+                {
+                return NotFound();
+                }
+            catch(DbConcurrenyException f)
+                {
+                return BadRequest();
+                }
             }
         }
     }
